e8dfcfb9841c245fce699d3b5bdec44ba9f03ad4
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-07-16 10:34:14 -0300

    upd tests

--- frete_test.go
+++ frete_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestSimpleRequest(t *testing.T) {
 	StupidSingleServiceMode = true
-	r := NewFreteRequest("01243000", "01232000")
+	r := NewFreteRequest("01243000", "65299970")
 	resp, err := CalcularFrete(context.Background(), r)
 	if err != nil {
 		t.Fail()
